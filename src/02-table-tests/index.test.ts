3ce5cbab55216403ddbc1f615fdec30447af938c
Pavel Sheshko <psheshko@twelvedevs.com>
2023-06-27 19:31:16 +0300

    feat: table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -8,7 +8,25 @@ const testCases = [
   { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 10, b: 5, action: Action.Subtract, expected: 5 },
   { a: 7, b: 4, action: Action.Subtract, expected: 3 },
+  { a: 2, b: 3, action: Action.Multiply, expected: 6 },
+  { a: 3, b: 3, action: Action.Multiply, expected: 9 },
+  { a: 4, b: 4, action: Action.Multiply, expected: 16 },
+  { a: 4, b: 4, action: Action.Divide, expected: 1 },
+  { a: 4, b: 2, action: Action.Divide, expected: 2 },
+  { a: 16, b: 4, action: Action.Divide, expected: 4 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 4, action: Action.Exponentiate, expected: 16 },
   // continue cases for other actions
+];
+
+const testCases2 = [
+  { a: 1, b: 2, action: 'Dancing' },
+  { a: 2, b: 2, action: 5 },
+  { a: '2', b: '2', action: '5' },
+  { a: 2, b: '2', action: '5' },
+  { a: '2', b: 2, action: '5' },
+  { a: '2', b: '2', action: Action.Add },
 ];
 
 describe('simpleCalculator', () => {
@@ -16,4 +34,12 @@ describe('simpleCalculator', () => {
     const result = simpleCalculator({ a, b, action });
     expect(result).toBe(expected);
   });
+
+  test.each(testCases2)(
+    'table as a variable with null result',
+    ({ a, b, action }) => {
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBeNull;
+    },
+  );
 });
