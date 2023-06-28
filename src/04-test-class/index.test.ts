8fc2b36122e4138003bbc6bdcefcc44d1bc03758
Pavel Sheshko <psheshko@twelvedevs.com>
2023-06-28 12:32:45 +0300

    feat: start implement 04-test-class

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,13 +1,21 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  getBankAccount,
+  BankAccount,
+  // TransferFailedError,
+  // SynchronizationFailedError,
+  InsufficientFundsError,
+} from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const bankAccount = getBankAccount(500);
+    expect(bankAccount).toBeInstanceOf(BankAccount);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const bankAccount = getBankAccount(500);
+    expect(() => bankAccount.withdraw(600)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
